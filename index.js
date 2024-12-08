b63f1d7d6f0d3e35f28693eb2c5deda280bcf7d3
rimonc7 <rimonc7@gmail.com>
2024-12-08 13:26:20 +0600

    subscriber api

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 app.use(cors());
@@ -30,6 +30,7 @@ async function run() {
 
         const database = client.db('moviesDB');
         const movieCollection = database.collection('movies')
+        const subscriberCollection = database.collection('subscribers')
 
         app.post('/movies', async (req, res) => {
             const movie = req.body;
@@ -37,12 +38,30 @@ async function run() {
             res.send(result)
 
         })
+        app.post('/subscribers', async (req, res) => {
+            const subscriber = req.body;
+            const result = await subscriberCollection.insertOne(subscriber);
+            res.send(result)
+        })
+
+        app.get('/subscribers', async (req, res) => {
+            const cursor = subscriberCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
         app.get('/movies', async (req, res) => {
             const limit = parseInt(req.query.limit) || 0;
             const cursor = movieCollection.find().sort({ rating: -1 }).limit(limit);
             const result = await cursor.toArray();
             res.send(result);
+        })
+
+        app.get('/movies/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const movie = await movieCollection.findOne(query);
+            res.send(movie)
         })
 
         await client.db("admin").command({ ping: 1 });
